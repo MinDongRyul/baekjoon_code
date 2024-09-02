21ec14361d7bd71e85d3c3a144f8f607339e699c
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-09-02 18:11:40 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,8 @@
-num = 1
-dict_ = {}
-for idx in range(10):
-    dict_[str(idx)] = 0
-
+num = 1    
 for _ in range(3):
     num *= int(input())
-
-for num_ in str(num):
-    dict_[num_] += 1
-
-for key in dict_:
-    print(dict_[key])
+    
+num = list(map(int, str(num)))
+    
+for num_ in range(0, 10):
+    print(num.count(num_))
