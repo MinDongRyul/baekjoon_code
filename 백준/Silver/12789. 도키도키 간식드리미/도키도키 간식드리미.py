d67c7098a185e6d4c27048e25a0842eec3bd19c7
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-09-04 23:26:16 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -2,16 +2,19 @@ n = int(input())
 wait = list(map(int, input().split()))
 stack = [1001]
 last = []
+idx = 1
 
 while wait:
     first_wait = wait[0]
     last_stack = stack[-1]
-    if first_wait == min(stack+wait):
+    if first_wait == idx:
         last.append(first_wait)
         wait.remove(first_wait)
-    elif last_stack == min(stack+wait):
+        idx += 1
+    elif last_stack == idx:
         last.append(last_stack)
         stack.remove(last_stack)
+        idx += 1
     else:
         stack.append(first_wait)
         wait.remove(first_wait)
