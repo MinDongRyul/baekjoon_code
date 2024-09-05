4f4c632663258bf9628fc18bfdcc66557d535994
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-09-05 22:47:09 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,26 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 while True:
-    string = input()
+    string = input().rstrip()
     if string == '.':
         break
     stack = []
     yes_true = True
     for unit in string:
-        if unit.isalpha() or unit.isspace():
-            pass
-        elif unit == '(' or unit == '[':
+        if unit in '([':
             stack.append(unit)
-        elif unit == ')':
-            if not stack or stack[-1] == '[':
-                yes_true = False
-                break
-            else:
-                stack.pop()
-        elif unit == ']':
-            if not stack or stack[-1] == '(':
-                yes_true = False
-                break
-            else:
-                stack.pop()
+        elif (unit == ')' and (not stack or stack[-1] == '[')) or (unit == ']' and (not stack or stack[-1] == '(')):
+            yes_true = False
+            break
+        elif (unit == ')' and stack[-1] == '(') or (unit == ']' and stack[-1] == '['):
+            stack.pop()
+
     if yes_true and not stack:
         print('yes')
     else:
