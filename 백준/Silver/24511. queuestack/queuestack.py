bfbd8741600f883d51cb4e1f165288eb3acf6a62
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-09-07 11:38:36 +0900

    [Silver III] Title: queuestack, Time: 164 ms, Memory: 47144 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -4,10 +4,7 @@ raw = list(map(int, input().split()))
 M = int(input())
 C = list(map(int, input().split()))
 
-real_use = [raw[idx] for idx in range(N) if queue_stack[idx] == 0][::-1]
-result = []
-for unit in C:
-    real_use.append(unit)
+real_use = [raw[idx] for idx in range(N) if queue_stack[idx] == 0][::-1] + C
 
 for idx in range(M):
     print(real_use[idx], end = ' ')
