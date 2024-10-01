ff4dbd4e13fb3572c0ca82a56b90d6348b833f42
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-10-01 14:17:20 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,26 +1,13 @@
-def func(arr : int) -> int:
+from typing import List
+def func(arr : List) -> int:
     total = 0
     for arr_ in arr:
-        temp = 0
-        for idx in range(len(arr_) - 1):
-            cur, nex = arr_[idx], arr_[idx+1]
-            if cur == nex and cur != 'X':
-                temp += 1
-            elif temp > 0 and cur != nex:
-                temp = 0
-                total += 1
-        if temp > 0:
-            total += 1
+        total += len([bed for bed in ''.join(arr_).split('X') if len(bed) > 1])
     return total
 
 N = int(input())
-ver = []
-hori = [[0] * N for _ in range(N)]
+arr = []
 for _ in range(N):
-    ver.append(input())
+    arr.append(input())
     
-for x in range(N):
-    for y in range(N):
-        hori[x][y] = ver[y][x]
-    
-print(func(ver), func(hori))
+print(func(arr), func(zip(*arr)))
