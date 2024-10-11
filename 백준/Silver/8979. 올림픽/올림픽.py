06e1c208ae3206ca6e1ab40ce0d93a1a20497392
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-10-12 00:53:56 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,14 +1,34 @@
+import sys
+input = sys.stdin.readline
+
 def func(N : int, K : int) -> int:
     rank = 1
-    medal = [list(map(int, input().split())) for _ in range(N)]
-    medal = sorted(medal, key=lambda x : [x[1], x[2], x[3]], reverse=True)
-    
-    for idx in range(len(medal)-1):
-        if medal[idx][0] == K:
+    medal = [list(map(int, input().rstrip('\n').split())) for _ in range(N)]
+    for value in medal:
+        if value[0] == K:
+            save = value
             break
-        if medal[idx][1:] != medal[idx+1][1:]:
+    
+    for value in medal:
+        if value[1] > save[1]:
             rank += 1
+            continue
+        elif value[1] < save[1]:
+            continue
+        else:
+            if value[2] > save[2]:
+                rank += 1
+                continue
+            elif value[2] < save[2]:
+                continue
+            else:
+                if value[3] > save[3]:
+                    rank += 1
+                    continue
+                elif value[3] < save[3]:
+                    continue
+        
     return rank
 
-N, K = map(int, input().split())
+N, K = map(int, input().rstrip('\n').split())
 print(func(N, K))
