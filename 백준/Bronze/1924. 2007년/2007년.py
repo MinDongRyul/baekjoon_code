d258bb366cc7e367f987a4d16b43520cad6c2ea2
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2025-07-23 20:57:20 +0900

    [Bronze I] Title: 2007년, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,5 +1,5 @@
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days = 'SUN, MON, TUE, WED, THU, FRI, SAT, SUN'.split(', ')
+days = 'SUN, MON, TUE, WED, THU, FRI, SAT'.split(', ')
 x, y = map(int, input().split())
 
 print(days[(sum(months[:x-1]) + y) % 7])
